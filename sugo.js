9786b263c6c3dbdbcaa0255c87adc9c081481c27
kentaromiura <kentaromiura@gmail.com>
2014-02-13 20:29:25 +0000

    change the executable so it will work in osx also

---
+++ sugo.js
@@ -0,0 +1,114 @@
+var clint = require('clint')()
+var flow = require('finally')
+
+clint.command('--help', '-h', 'General usage information')
+clint.command('--full', '-f', 'Use all the features at the same time')
+
+clint.command('--typescript', '-ts', 'Starts typescript server')
+clint.command('--ecmascript6', '-es6', 'Starts es6 server')
+clint.command('--react', '-r', 'Starts react jsx server')
+
+clint.command('--port', '-p', '[port] change port, default 31337')
+clint.command('--ip', '-i', '[ip] change the ip, default 0.0.0.0')
+
+var options = {
+  polpetta: './node_modules/.bin/polpetta',
+  help: false,
+  react : false,
+  ts: false,
+  es6: false,
+  full: false,
+  none: true,
+  ip: '0.0.0.0',
+  port: 31337
+}
+var selections = 0;
+
+function fullerize(){
+  options.none = options.ts = options.es6 = options.react = false; options.full = true;
+}
+
+clint.on('command', function(name, value){
+  switch(name){
+    case '--port'       : options.port = value; break
+    case '--ip'         : options.ip = value; break
+    case '--help'       : options.help = true; break
+    case '--react'      : selections++; options.react = true; break
+    case '--typescript' : selections++; options.none = false; options.ts   = true; break
+    case '--ecmascript6': selections++; options.none = false; options.es6  = true; break
+    case '--full': selections++; fullerize(); break
+  }
+})
+
+clint.on('complete', function(){
+  console.log(clint.help(2, " : "))
+
+  if (options.help){
+    console.log('This is the help message')
+    process.exit(0)
+  }
+
+  var args = []
+  if (selections > 1){
+    console.log('You selected more than one options, switching to full featured server')
+    fullerize()
+  }
+  
+  if (options.none) {
+    console.log("You didn't select a server")
+    process.exit(2)
+  }
+
+  var spawn = require('child_process').spawn; 
+  var ƒ = flow();
+	  
+  if (options.ts) {
+    args.push('./ts')
+    var ts = require('typescript'),
+        need_fix = ts.TypeScriptCompiler == undefined
+
+	if (need_fix) {	
+	  ƒ.then(function(err){
+		var me = this
+	  	var fix = spawn('./fix_typescript.sh')  
+                fix.on('close', function(){
+			me.continue()
+		})
+		fix.stdout.on('data', function(data){
+			console.log(data.toString())
+		})
+	  })
+        }
+  }
+  if (options.es6)   args.push('./es6') 
+  if (options.react) args.push('./react')
+  if (options.full)  args.push('./full')
+
+  args.push(options.ip + ':' + options.port)
+
+
+var _code
+  ƒ.then(function(err){
+    var me = this
+    var prc = spawn(options.polpetta, args);
+    prc.on('close', function (code) {
+      _code = code
+      console.log('process exit code ' + code);
+      me.continue()
+
+    });
+
+
+    prc.stdout.on('data', function (data) {
+      var str = data.toString()
+      var lines = str.split(/(\r?\n)/g);
+      console.log(lines.join(""));
+    });
+
+  })
+  ƒ.finally(function(){
+      process.exit(_code)
+  })
+})
+
+clint.parse(process.argv.slice(2)) 
