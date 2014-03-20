c19f1fcbdd15f817c651bdd82939710054db5565
kentaromiura <kentaromiura@gmail.com>
2014-03-21 00:29:02 +0100

    adding coffeescript

--- sugo.js
+++ sugo.js
@@ -8,6 +8,7 @@ clint.command('--help', '-h', 'General usage information')
 clint.command('--full', '-f', 'Use all the features at the same time')
 
 clint.command('--typescript', '-ts', 'Starts typescript server')
+clint.command('--coffeescript', '-cs', 'Starts the coffeescript server')
 clint.command('--ecmascript6', '-es6', 'Starts es6 server')
 clint.command('--react', '-r', 'Starts react jsx server')
 clint.command('--wrapup', '-w', 'Starts the wrapup server')
@@ -16,7 +17,7 @@ clint.command('--open', '-o', 'opens the directory where polpetta runs')
 clint.command('--port', '-p', '[port] change port, default 31337')
 clint.command('--ip', '-i', '[ip] change the ip, default 0.0.0.0')
 clint.command('--path', '-p', 'Path where to copy .htaccess and run polpetta')
- 
+
 var options = {
   polpetta: './node_modules/.bin/polpetta',
   help: false,
@@ -24,6 +25,7 @@ var options = {
   ts: false,
   es6: false,
   wrapup: false,
+  coffee: false,
   full: false,
   none: true,
   ip: '0.0.0.0',
@@ -34,7 +36,7 @@ var options = {
 var selections = 0;
 
 function fullerize(){
-  options.none = options.wrapup = options.ts = options.es6 = options.react = false; options.full = true;
+  options.none = options.wrapup = options.ts = options.es6 = options.react = options.coffee = false; options.full = true;
 }
 
 function set(option){
@@ -45,15 +47,16 @@ function set(option){
 
 clint.on('command', function(name, value){
   switch(name){
-    case '--open'       : options.open = true; break
-    case '--port'       : options.port = value; break
-    case '--ip'         : options.ip = value; break
-    case '--help'       : options.help = true; break
+    case '--open'        : options.open = true; break
+    case '--port'        : options.port = value; break
+    case '--ip'          : options.ip = value; break
+    case '--help'        : options.help = true; break
 
-    case '--wrapup'     : set('wrapup'); break
-    case '--react'      : set('react'); break
-    case '--typescript' : set('ts'); break
-    case '--ecmascript6': set('es6'); break
+    case '--wrapup'      : set('wrapup'); break
+    case '--react'       : set('react'); break
+    case '--typescript'  : set('ts'); break
+    case '--ecmascript6' : set('es6'); break
+    case '--coffeescript': set('coffee'); break
 
     case '--full': selections++; fullerize(); break
     case '--path': options.path = value; break;
@@ -80,26 +83,26 @@ clint.on('complete', function(){
     if(!fs.existsSync(options.path)){
       console.log("The path "+options.path+" does not exist")
       process.exit(3)
-    }	
-  }  
- 
+    }
+  }
+
   if (options.none) {
     console.log("You didn't select a server")
     process.exit(2)
   }
 
-  var spawn = require('child_process').spawn; 
-  
-	  
+  var spawn = require('child_process').spawn;
+
+
   if (options.full || options.ts) {
     if (options.ts) args.push('./ts')
     var ts = require('typescript'),
         need_fix = ts.TypeScriptCompiler == undefined
 
-	if (need_fix) {	
+	if (need_fix) {
 	  ƒ.then(function(err){
 		var me = this
-	  	var fix = spawn('./fix_typescript.sh')  
+	  	var fix = spawn('./fix_typescript.sh')
                 fix.on('close', function(){
 			me.continue()
 		})
@@ -110,10 +113,11 @@ clint.on('complete', function(){
         }
   }
 
-  if (options.es6)    args.push('./es6') 
+  if (options.es6)    args.push('./es6')
   if (options.react)  args.push('./react')
   if (options.full)   args.push('./full')
   if (options.wrapup) args.push('./wrup')
+  if (options.coffee) args.push('./coffee')
 
   args.push(options.ip + ':' + options.port)
 
@@ -131,7 +135,7 @@ clint.on('complete', function(){
         if(err){
           console.log('Error while copying node_modules', err)
           process.exit(6)
-        } 
+        }
         me.continue()
       })
     })
@@ -152,8 +156,8 @@ clint.on('complete', function(){
         me.continue()
       });
       read.pipe(write);
-      
-    })  
+
+    })
   }
 
 var _code
@@ -181,4 +185,4 @@ var _code
   })
 })
 
-clint.parse(process.argv.slice(2)) 
+clint.parse(process.argv.slice(2))
